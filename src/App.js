377220b5b5ae2d6aa91ccfc05b2e9c7fa41b7c92
MJ0629 <ymj910629@naver.com>
2024-03-16 12:21:08 +0900

    d
    d

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [category, setCategory] = useState("");
+  const [canSave, setCanSave] = useState(false);
+
+  useEffect(() => {
+    if (
+      title === "" || title.length <= 0 || title.trim() === "" ||
+      body === "" || body.length <= 0 || body.trim() === "" ||
+      category === "" || category.length <= 0 || category.trim() === ""
+    ) {
+      setCanSave(false)
+    } else {
+      setCanSave(true)
+    }
+  }, [title, body, category])
   return (
     <div className="App">
-      <div className="container">
-        <header id="header">
-          <div className="nav">
-            <div className="menuButton">
-              <span className="menuLine"></span>
-              <span className="menuLine"></span>
-              <span className="menuLine"></span>
-            </div>
-            {/* menuButton */}
-            <h1>MJ's DEVELOP BLOG</h1>
+      <div className="w-full h-full min-h-screen">
+        <header className="header bg-[#000]">
+          <div className="nav max-w-screen-xl mx-auto w-11/12 py-4 justify-between">
+            <h1 className="tw-text-center text-white text-2xl font-black font-serif">MJ's BLOG</h1>
+            <button type="button" className="searchBtn text-white">버튼있어요{canSave}</button>
           </div>
           {/* navs */}
-          <button type="button" className="searchBtn">0</button>
         </header>
-
       </div>
+      {/* 네비게이션 컴포넌트 */}
+
+
     </div>
   );
 }
